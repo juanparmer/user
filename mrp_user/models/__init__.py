e5772eb03c325f7259d39c6eb8540e8ee1f98288
Juan Arcos <juanparmer@gmail.com>
2024-02-02 05:49:35 +1000

    16.0 [ADD] add repository 16.0

--- mrp_user/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import account_analytic
-from . import mrp_bom
-from . import mrp_production
-from . import mrp_workcenter
-from . import mrp_workorder
-# from . import res_company
---
+++ sh_customer_statement/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+
+from . import res_users
+from . import sh_customer_mail_history
+from . import res_config_settings
+from . import res_partner
+from . import sh_customer_statement_config
+from . import sh_customer_mass_update
